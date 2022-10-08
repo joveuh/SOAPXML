8e4652ea414a1c56179ec3d0e7a7abe17c17ad38
Uzair <uhass924@gmail.com>
2022-10-08 18:57:15 -0400

    Application context for component management and getBean

--- src/main/java/com/springboot/spring/learningspring/LearningspringApplication.java
+++ src/main/java/com/springboot/spring/learningspring/LearningspringApplication.java
@@ -10,13 +10,16 @@ public class LearningspringApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(LearningspringApplication.class, args);
 
-		SuperContraGame game = new SuperContraGame();
-		GameRunner runner = new GameRunner(game);
-		runner.run();
+		// SuperContraGame game = new SuperContraGame();
+		// GameRunner runner = new GameRunner(game);
+		// runner.run();
 
-		MarioGame game2 = new MarioGame();
-		GameRunner runner2 = new GameRunner(game2);
-		runner2.run();
+		// MarioGame game2 = new MarioGame();
+		// GameRunner runner2 = new GameRunner(game2);
+		// runner2.run();
+
+		GameRunner runner = context.getBean(GameRunner.class);
+		runner.run();
 	}
 
 }
