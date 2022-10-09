f84327cc82c5116b367ea5acaf8398cb9ce01497
Uzair <uhass924@gmail.com>
2022-10-09 17:58:54 -0400

    Using @Primary

--- src/main/java/com/springboot/spring/learningspring/SuperContraGame.java
+++ src/main/java/com/springboot/spring/learningspring/SuperContraGame.java
@@ -1,5 +1,10 @@
 package com.springboot.spring.learningspring;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class SuperContraGame implements GamingConsole{
     public void up(){
         System.out.println("SuperContraGame up");
