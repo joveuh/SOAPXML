3661b71b3c4fb9d7918d574bfe3d1a1bed2499ff
Uzair H <uzair@ibm.com>
2022-10-11 16:48:40 -0400

    Adding profiles

---
+++ src/main/java/com/springboot/spring/learningspring/Course.java
@@ -0,0 +1,13 @@
+package com.springboot.spring.learningspring;
+
+public class Course {
+    int id;
+    String name;
+    String author;
+
+    public Course(int i, String name, String author){
+        this.id = i;
+        this.name = name;
+        this.author = author;
+    }
+}
---
+++ src/main/java/com/springboot/spring/learningspring/TestAPI.java
@@ -0,0 +1,20 @@
+package com.springboot.spring.learningspring;
+
+import java.util.Arrays;
+import java.util.List;
+
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// @RestController
+public class TestAPI {
+
+    // @RequestMapping("/courses")
+    public List<Course> outputAllCourses(){
+        return Arrays.asList(
+            new Course(1, "course1", "uzair"),
+            new Course(2, "course2", "uzair"),
+            new Course(3, "course3", "uzair")
+        );
+    }
+}
